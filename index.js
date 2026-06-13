bd0783de25a430b24fa4ddab5d42f5c10166632a
ViRaX5 <amit505r@gmail.com>
2026-06-13 16:19:50 +0300

    basic stuff

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 const express = require('express')
+const app = express()
+
+app.get("/", (req, res) => {
+    res.send("This is local host 3000")
+})
+
+
+app.listen(3000, () => {
+    console.log("server is running on localhost:3000")
+})
+
+
