66fa0793e10787f464c54cffca8d514064681ede
ViRaX5 <amit505r@gmail.com>
2026-06-16 15:23:22 +0300

    Finished the login page backend and connected it to the frontend

--- Modules/loginModule.js
+++ Modules/loginModule.js
@@ -1,12 +1,41 @@
-const tempDB = {"firstname": "Amit", "lastname": "Lachmann", "email": "amit505r@gmail.com", "password": "HelloWorld"};
+const tempDB = { "firstname": "Amit", "lastname": "Lachmann", "email": "amit505r@gmail.com", "password": "HelloWorld" };
 
-function validateSignUp(firstname, lastname, email, password, repeatPassword)
-{
+function validateSignUp(firstname, lastname, email, password, repeatPassword) {
     let errors = [];
 
-    if(!firstname) errors.push()
+    if (!firstname) { errors.push({ field: 'firstname', msg: 'First name is required' }) }
+    if (!lastname) {errors.push({field: 'lastname', msg: 'Last name is required'})}
+    if (!email) {errors.push({field: 'email', msg: 'Email is required'})}
+    if (email === tempDB.email) {errors.push({field: 'email', msg: 'This email already has an account'})}
+    if (!password) {errors.push({field: 'password', msg: 'Password is required'})}
+    else if (password.length < 8) {errors.push({field: 'password', msg: 'Password must contain at least 8 characters'})}
+    if (password !== repeatPassword)
+    {
+        errors.push({field: 'repeatPassword', msg: 'Password and repeat password do not match'})
+        errors.push({field: 'password', msg: 'Password and repeat password do not match'})
+    }
+    
+    return errors
+}
+
+function validateLogin(email, password) {
+    let errors = []
+
+    if (!email) errors.push({ field: 'emailLogin', msg: 'Email is required' });
+    if (email !== tempDB.email && email !== '') {
+        errors.push({ field: 'emailLogin', msg: 'Wrong email' });
+    } 
+    
+    if (!password) {
+        errors.push({ field: 'passwordLogin', msg: 'Password is required' });
+    } else if (password !== tempDB.password && password !== '') {
+        errors.push({ field: 'passwordLogin', msg: 'Wrong password' });
+    }
+
+    return errors;
 }
 
 module.exports = {
-    validateSignUp
+    validateSignUp,
+    validateLogin
 }
--- index.js
+++ index.js
@@ -7,6 +7,44 @@ const port = process.env.PORT || 8080
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use((req, res, next) => {
+    res.set({
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
+        'Content-Type': 'application/json'
+    })
+    next()
+})
+
+app.post('/api/signup', (req, res) => {
+    const { firstname, lastname, email, password, repeatPassword } = req.body
+
+    const errors = loginSignupModule.validateSignUp(firstname, lastname, email, password, repeatPassword)
+    
+    if (errors.length > 0)
+    {
+        return res.status(400).json({success: false, errors})
+    }
+
+    // data is valid TODO add to database
+
+    res.json({ success: true, redirect: './campaignList.html'})
+    // look into json web tokens to keep track of which account it is that is logged in
+})
+
+app.post('/api/login', (req, res) => {
+    const { email, password } = req.body;
+    
+    const errors = loginSignupModule.validateLogin(email, password);
+
+    if (errors.length > 0) {
+        return res.status(400).json({ success: false, errors });
+    }
+
+    res.json({ success: true, redirect: './campaignList.html' });
+    // look into json web tokens to keep track of which account it is that is logged in
+})
 
 app.get("/", (req, res) => {
     res.send(`This is localhost:${port}`)
